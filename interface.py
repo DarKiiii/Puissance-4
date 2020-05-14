69443c4bb4129ab5e4025123d13ab0d8f6b0fead
DarKiiii <lucasd0104@gmail.com>
2020-05-14 10:07:43 +0200

    the big update

--- interface.py
+++ interface.py
@@ -4,6 +4,22 @@ from tkinter import *; import random; from PIL import Image, ImageTk;
 # CONFIG
 w, h = 1280, 720
 
+def none():
+    return None
+
+def img_resize(img_name, size):
+    img = Image.open(img_name);
+    width, height = img.size;
+    if size["w"] and size["h"]:
+        resized_img = img.resize((size["w"], size["h"]));
+    elif size["w"]:
+        resized_img = img.resize((size["w"], round(height*(size["w"]/width))));
+    elif size["h"]:
+        resized_img = img.resize((round(width*(size["h"]/height)), size["h"]));
+    else:
+        print("error appened while resizing", img_name, "image")
+    return ImageTk.PhotoImage(resized_img, Image.ANTIALIAS);
+
 # ---------- MODULES -----------
 class Root:
     '''
@@ -17,8 +33,12 @@ class Root:
         self.tk.resizable(False, False)
         self.buts = {};
 
-    def new_but(self, name, img, size, pos, onclick, args=()):
+    def new_but(self, name, img, size, pos, onclick=none, args=()):
         self.buts[name] = ImgButton(self, img, size, pos, onclick, args);
+
+    def lift_widgets(self):
+        for k, w in self.buts.items():
+            w.tk.lift()
 
     def destroy(self):
         self.tk.destroy()
@@ -28,7 +48,7 @@ class Root:
 
 class App:
     '''
-    tkinter label that can be used as non direct parent for other widgets
+    tkinter label that can be used as direct parent for other widgets
     img     : path to image
     '''
     def __init__(self, master, img):
@@ -38,29 +58,33 @@ class App:
         self.tk.pack();
         self.buts = {};
         self.entry = {};
-        self.color = "red"
 
-    def new_but(self, name, img, size, pos, onclick, args=()):
+    def new_but(self, name, img, size, pos, onclick=none, args=()):
         self.buts[name] = ImgButton(self, img, size, pos, onclick, args);
 
-    def new_text( self,name,pos,size ):
-        self.entry[name] = ZoneSaisie(self,size,pos);
+    def new_entry(self, name, size, pos, font=("Helvetica",10), bg='#FFF', default=""):
+        self.entry[name] = TxtInput(self, size, pos, font, bg, default);
 
-    def destroy_buts( self ):
-        button = self.buts["color"]
-        button.tk.destroy()
-        if self.color == "red":
-            self.new_but( "color", "color_yel.png", {"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.destroy_buts )
-            self.color = "yel"
-        else:
-            self.new_but( "color", "color_red.png", {"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.destroy_buts )
-            self.color = "red"
+    def lift_widgets(self):
+        for k, w in self.buts.items():
+            w.tk.lift();
+        for k, w in self.entry.items():
+            w.tk.lift();
+        self.master.lift_widgets()
 
-
+    # def destroy_buts( self ):
+    #     button = self.buts["color"]
+    #     button.tk.destroy();
+    #     if self.color == "red":
+    #         self.new_but( "color", "color_yel.png", {"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.destroy_buts );
+    #         self.color = "yel"
+    #     else:
+    #         self.new_but( "color", "color_red.png", {"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.destroy_buts );
+    #         self.color = "red";
 
     def destroy(self):
-        self.tk.destroy()
-        del(self)
+        self.tk.destroy();
+        del(self);
 
 class ImgButton:
     '''
@@ -70,25 +94,27 @@ class ImgButton:
     size    : dictionary with real size of the image
     pos     : args for tk.place function
     '''
-    def __init__(self, master, img, size, pos, onclick, args=()):
-        self.master = master
-        self.img = ImageTk.PhotoImage(Image.open(img).resize((round(size["w"]*(w/1920)), round(size["h"]*(h/1080))), Image.ANTIALIAS));
+    def __init__(self, master, img, size, pos, onclick=none, args=()):
+        self.master = master;
+        self.img = img_resize(img, size)
         self.tk = Label(master.tk, image=self.img, bd=0);
         self.tk.bind("<ButtonPress-1>", lambda event : onclick(*args));
         self.tk.place(pos);
 
     def destroy(self):
         self.tk.destroy();
-        del(self)
+        del(self);
 
 
-class ZoneSaisie:
+class TxtInput:
 
-    def __init__(self,master,size,pos):
-        self.master = master
-        self.tk = Entry(master.tk,bd=0,bg='#400000',font = ("Helvetica",35));
-        self.tk.place(w = 250, h =50,relx=0.41,rely=0.384);
+    def __init__(self, master, size, pos, font=("Helvetica",10), bg='#FFF', default=""):
+        self.master = master;
+        self.txtvar = StringVar();
+        self.txtvar.set(default);
+        self.tk = Entry(master.tk, bd=0, bg=bg, width=size["w"], font=font, textvariable=self.txtvar);
+        self.tk.place(pos);
 
     def destroy(self):
         self.tk.destroy();
-        del(self)
+        del(self);
--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 '''
 from grid import *; from interface import *;
 
+# {"w" :round($$*(w/1920)), "h" :round($$*(h/1080))} ## sample for buttons size
+main_but_size = {"w" :round(474*(w/1920)), "h" :round(134*(h/1080))};
+
 class Game():
     '''
     the game
     '''
     def __init__( self ):
         self.root = Root(w,h);
-        self.root.new_but( "close", "close_button.png", {"w" :54, "h" :54}, {"anchor" :NE, "x" :w, "y" :0}, self.root.destroy);
+        self.root.new_but( "close", "close_button.png", {"w" :36, "h" :36}, {"anchor" :NE, "x" :w, "y" :0}, self.root.destroy);
         self.menu = None
         self.main_menu();
 
@@ -17,35 +20,32 @@ class Game():
         if self.menu:
             self.menu.destroy();
         self.menu=App( self.root, "menu.png" );
-        self.menu.new_but( "solo", "solo_button.png", {"w" :474, "h" :134}, {"anchor" :N, "relx" :0.5, "rely" :0.4}, self.menu_solo );
-        self.menu.new_but( "multi", "multi_button.png", {"w" :474, "h" :134}, {"anchor" :N, "relx" :0.5, "rely" :0.55},  print );
-        self.menu.new_but( "rules", "rules_button.png", {"w" :474, "h" :134}, {"anchor" :N, "relx" :0.5, "rely" :0.73}, self.rules_menu );
-        self.root.buts["close"].tk.lift()
+        self.menu.new_but( "solo", "solo_button.png", main_but_size, {"anchor" :N, "relx" :0.5, "rely" :0.4}, self.solo_menu );
+        self.menu.new_but( "multi", "multi_button.png", main_but_size, {"anchor" :N, "relx" :0.5, "rely" :0.55},  print );
+        self.menu.new_but( "rules", "rules_button.png", main_but_size, {"anchor" :N, "relx" :0.5, "rely" :0.73}, self.rules_menu );
+        self.menu.lift_widgets();
         self.menu.tk.mainloop();
 
     def rules_menu( self ):
         if self.menu:
             self.menu.destroy();
         self.menu=App( self.root, "bg.png" );
-        self.menu.new_but('rules_png',"rules.png", {"w" :1869, "h" :693}, {"anchor" :N, "relx" :0.5, "rely" :0.1},print)
-        self.menu.new_but( "rules", "menu_button.png", {"w" :474, "h" :134}, {"anchor" :N, "relx" :0.5, "rely" :0.73}, self.main_menu );
-        self.root.buts["close"].tk.lift()
+        self.menu.new_but('rules_png',"rules.png", {"w" :round(1869*(w/1920)), "h" :round(693*(h/1080))}, {"anchor" :N, "relx" :0.5, "rely" :0.1})
+        self.menu.new_but( "rules", "menu_button.png", main_but_size, {"anchor" :N, "relx" :0.5, "rely" :0.73}, self.main_menu );
+        self.menu.lift_widgets();
         self.menu.tk.mainloop();
 
 
-    def menu_solo( self ):
+    def solo_menu( self ):
         if self.menu :
             self.menu.destroy();
-        self.menu=App( self.root, "font-solo.png" );
-        self.menu.new_text("Pseudo",{"w" :250, "h" :50},{"x" :0.5, "y" :0.73});
-        self.menu.new_but("color","color_red.png",{"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.menu.destroy_buts)
-        self.menu.new_but( "start", "start_button.png", {"w" :474, "h" :134}, {"anchor" :N, "relx" :0.49, "rely" :0.73},print )
-        self.menu.new_but( "back", "back_button.png", {"w" :166, "h" :134}, {"anchor" :N, "relx" :0.05, "rely" :0.05},self.main_menu )
+        self.menu=App( self.root, "solo-bg.png" );
+        self.menu.new_entry("playername", {"w" :14},{"relx" :0.4125, "rely" :0.405}, ("Helvetica",27), "#400000", "Nom du joueur");
+        # self.menu.new_but("color","color_red.png",{"w" :250, "h" :50}, {"anchor" :N, "relx" :0.515, "rely" :0.522},self.menu.destroy_buts)
+        self.menu.new_but( "start", "start_button.png", main_but_size, {"anchor" :N, "relx" :0.49, "rely" :0.73},print )
+        self.menu.new_but( "back", "back_button.png", {"w" :round(166*(w/1920)), "h" :round(134*(h/1080))}, {"anchor" :N, "relx" :0.05, "rely" :0.05},self.main_menu )
+        self.menu.lift_widgets();
         self.menu.tk.mainloop();
-
-
-
-
 
 if __name__ == '__main__':
     game = Game();
